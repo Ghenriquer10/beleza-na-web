74385d2279eb8150c1961aa5fe6c27b3192e25d9
Gustavo Henrique Nogueira <61561169+Ghenriquer10@users.noreply.github.com>
2022-07-07 17:44:48 -0300

    Update style.js

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.header`
     width: 100%;
     height: 100px;
-    border: 1px solid black;
     display: flex;
     align-items: center;
     justify-content: center;
